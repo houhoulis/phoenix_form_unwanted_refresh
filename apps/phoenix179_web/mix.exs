8d2999fead511cbcc15303c07fbd4c9520545040
Chris Houhoulis <chris@chrishouhoulis.com>
2023-11-19 16:12:39 -0500

    Upgrade phoenix, phoenix_* including phoenix_live_view.

--- apps/phoenix179_web/mix.exs
+++ apps/phoenix179_web/mix.exs
@@ -36,13 +36,13 @@ defmodule Phoenix179Web.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "1.7.9"},
+      {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
-      {:phoenix_html, "3.3.3"},
+      {:phoenix_html, "~> 3.3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "0.18.18"},
+      {:phoenix_live_view, "~> 0.20.1"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
+      {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
